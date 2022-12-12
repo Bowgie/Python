98934cec93d264d306a46d2b99cf0495cd3c985a
Bowgie <danny.hardawar@gmail.com>
2022-12-12 14:52:49 -0500

    Exercise from PythonProjects
    Chapter 1

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Print "Hello, world!" on the screen:
+print('Hello, world!')
+# Ask the user for their name:
+print('What is your name?')
+# Get the user's name from their keyboard input:
+name = input()
+# Greet the user by their name:
+print('Hello, ' + name)
